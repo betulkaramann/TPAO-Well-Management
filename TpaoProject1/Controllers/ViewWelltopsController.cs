9acbcbf81cc222504028e2c94f05b6a0c149bdd9
cavitesadimer <101523290+cavitesadimer@users.noreply.github.com>
2023-08-23 14:57:11 +0300

    Update ViewWelltopsController.cs

--- TpaoProject1/Controllers/ViewWelltopsController.cs
+++ TpaoProject1/Controllers/ViewWelltopsController.cs
@@ -1,25 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
-using TpaoProject1.Areas.Identity.Data;
-using TpaoProject1.Data;
-using TpaoProject1.Model;
-using TpaoProject1.Models;
+using TpaoWebApp.Areas.Identity.Data;
+using TpaoWebApp.Data;
+using TpaoWebApp.Model;
+using TpaoWebApp.Models;
 using X.PagedList;
 
-namespace TpaoProject1.Controllers
+namespace TpaoWebApp.Controllers
 {
     public class ViewWelltopsController : BaseController
     {
         private readonly DatabaseContext _dbContext;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly MapsGeocodingService _geocodingService;
+
         public ViewWelltopsController(DatabaseContext dbContext, UserManager<ApplicationUser> userManager, MapsGeocodingService geocodingService)
         {
             _dbContext = dbContext;
             _userManager = userManager;
             _geocodingService = geocodingService;
-
         }
 
         [HttpGet]
@@ -27,30 +27,32 @@ namespace TpaoProject1.Controllers
         {
             ViewBag.ActionName = "AddWellTop";
             ViewBag.ButtonText = "Kaydet";
+
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> AddWellTop(WellTop model)
-        {
+        {   //Yeni kuyu ekleme işlemleri
             ViewBag.ActionName = "AddWellTop";
             ViewBag.ButtonText = "Kaydet";
             var WellTopList = _dbContext.WellTops.ToList();
+
             if (ModelState.IsValid)
             {
                 int counter = 0;
                 string city = null;
-                double lati = double.Parse(model.Latitude);//enlem- paralel 36-42
-                double longi = double.Parse(model.Longitude);//boylam - meridyen 26-45
+                double lati = double.Parse(model.Latitude);
+                double longi = double.Parse(model.Longitude);
                 string apiKey = "AIzaSyDU_pWP66-BTzvW7AnEcQRSaBPutMzWxU4";
+
                 string geocodingData = await _geocodingService.GetGeocodingData(lati, longi, apiKey);
                 try
                 {
                     if (!string.IsNullOrEmpty(geocodingData))
-                    {
+                    {   //Koordinatlara göre şehir ataması
                         while (city == null)
                         {
-
                             JObject jsonObject = JObject.Parse(geocodingData);
                             city = jsonObject["results"][counter]["address_components"]
                                                  .FirstOrDefault(c => c["types"].Any(t => t.ToString() == "locality" || t.ToString() == "administrative_area_level_1"))?["long_name"]?.ToString();
@@ -61,6 +63,7 @@ namespace TpaoProject1.Controllers
                         model.City = city;
                     }
                     var user = await _userManager.GetUserAsync(User);
+
                     model.UserId = user.Id;
                     var numUserId = model.UserId.ToString();
 
@@ -71,6 +74,7 @@ namespace TpaoProject1.Controllers
                     var City = model.City;
                     var InsertionDate = DateTime.Now;
                     var UpdateDate = DateTime.Now;
+
                     WellTop welltop = new WellTop { UserId = numUserId, Name = Name, Latitude = Latitude, Longitude = Longitude, WellTopType = WellTopType, City = City, InsertionDate = InsertionDate, UpdateDate = UpdateDate };
                     var name = welltop.Name;
                     var longitude = welltop.Longitude;
@@ -80,62 +84,37 @@ namespace TpaoProject1.Controllers
                     {
                         _dbContext.WellTops.Add(welltop);
                     }
-                    else
+                    else 
                     {
                         BasicNotification("Koordinatlarınızı tekrar giriniz...", NotificationType.Error, "Seçtiğiniz koordinatlarda kuyu bulunmaktadır");
                         return View();
                     }
+                    
                     BasicNotification("Anasayfaya yönlendiriliyorsunuz...", NotificationType.Success, "Kuyu Başarıyla Eklendi!");
                     await _dbContext.SaveChangesAsync();
+
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
+
                     BasicNotification("Koordinatlarınızı kontrol ediniz..", NotificationType.Question, "Türk deniz sahasını aştınız!");
                 }
-                return RedirectToAction("AddedWell", "ViewWelltops");
+
+                return RedirectToAction("AddedWelltops", "Home");
             }
-
-
             return View();
         }
-        public async Task<IActionResult> AddedWell()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            var WellTopList = _dbContext.WellTops.ToList();
-
-
-            if (User.IsInRole("Admin"))
-            {
-                WellTopList = _dbContext.WellTops.ToList();
-            }
-            else
-            {
-                WellTopList = _dbContext.WellTops.Where(w => w.UserId == user.Id).ToList();
-            }
-
-
-            var users = _dbContext.Users.ToList();
-
-
-            var viewModel = new UserRolesViewModel
-            {
-                Kullanicilar = users,
-                Kuyular = WellTopList
-
-            };
-            return View(viewModel);
-        }
+        
         public bool IsLocationExists(string longitude, string latitude)
-        {
+        {   //Verilen koordinatlarla eşleşen kuyu varlığını kontrol etme
             return _dbContext.WellTops.Any(u => u.Latitude == latitude) && _dbContext.WellTops.Any(u => u.Longitude == longitude);
         }
 
         public async Task<IActionResult> MainPage(int page = 1, int pageSize = 50)
         {
             IPagedList<WellTop> data = null;
+            
             var user = await _userManager.GetUserAsync(User);
             var WellTopList = _dbContext.WellTops.ToList();
-
 
             if (User.IsInRole("Admin"))
             {
@@ -146,9 +125,10 @@ namespace TpaoProject1.Controllers
                 WellTopList = _dbContext.WellTops.Where(w => w.UserId == user.Id).ToList();
             }
 
-
             var users = _dbContext.Users.ToList();
+            
             List<WellTop> welltop = new();
+            //Proje klasöründe var olan çoklu random kuyuları haritada optimize şekilde gösterme 
             using (var reader = new StreamReader(@"randomKuyuVerisi.csv"))
             {
                 bool flag = false;
@@ -162,14 +142,15 @@ namespace TpaoProject1.Controllers
                         var name = values[0];
                         var lngi = values[1];
                         var lati = values[2];
-                        string real_type = "";
-                        var type_number = Int32.Parse(values[3]);
-                        if (type_number == 0)
-                            real_type = "arama";
-                        else if (type_number == 1)
-                            real_type = "tespit";
-                        else if (type_number == 2)
-                            real_type = "üretim";
+                        string type = "";
+                        var typeNumber = Int32.Parse(values[3]);
+                        if (typeNumber == 0)
+                            type = "arama";
+                        else if (typeNumber == 1)
+                            type = "tespit";
+                        else if (typeNumber == 2)
+                            type = "üretim";
+                        
 
                         WellTop new_well = new()
                         {
@@ -177,13 +158,12 @@ namespace TpaoProject1.Controllers
                             Latitude = lati,
                             Longitude = lngi,
                             Name = name,
-                            WellTopType = real_type
+                            WellTopType = type
                         };
 
                         welltop.Add(new_well);
                         data = welltop.ToList().ToPagedList(page, pageSize);
                     }
-
                     flag = true;
                 }
             }
@@ -193,18 +173,22 @@ namespace TpaoProject1.Controllers
                 Kullanicilar = users,
                 Kuyular = data,
                 MapKuyular = welltop,
-                PageSize = pageSize,
+                PageSize = pageSize
             };
             return View(viewModel);
         }
+       
         public async Task<IActionResult> Delete(int id)
         {
+            DeleteNotification("Silinen Kuyu Geri Getirilmez!", NotificationType.Warning, "Emin Misiniz?");
+
             ViewBag.ActionName = "Delete";
             ViewBag.ButtonText = "Sil";
+
             var kuyu = _dbContext.WellTops.Find(id);
             _dbContext.Remove(kuyu);
             _dbContext.SaveChanges();
-            return RedirectToAction("AddedWell", "ViewWelltops");
+            return RedirectToAction("AddedWelltops", "Home");
         }
         [HttpGet]
         public async Task<IActionResult> Update(int id)
@@ -214,6 +198,7 @@ namespace TpaoProject1.Controllers
             var kuyu = _dbContext.WellTops.Find(id);
             var updateUserId = kuyu.UserId;
             TempData["userid"] = updateUserId;
+            TempData["type"] = kuyu.WellTopType;
             return View(kuyu);
         }
         [HttpPost]
@@ -224,41 +209,43 @@ namespace TpaoProject1.Controllers
 
             double lati = double.Parse(welltop.Latitude);
             double longi = double.Parse(welltop.Longitude);
+            string welltopType = welltop.WellTopType;
+            if(welltopType == null)
+            {
+                welltop.WellTopType = TempData["type"].ToString();
+            }
             string apiKey = "AIzaSyDU_pWP66-BTzvW7AnEcQRSaBPutMzWxU4";
 
             string geocodingData = await _geocodingService.GetGeocodingData(lati, longi, apiKey);
             try
             {
-                if (!string.IsNullOrEmpty(geocodingData))
-                {
-
-                    while (city == null)
-                    {
-                        JObject jsonObject = JObject.Parse(geocodingData);
-                        city = jsonObject["results"][counter]["address_components"]
-                                                             .FirstOrDefault(c => c["types"].Any(t => t.ToString() == "locality" || t.ToString() == "administrative_area_level_1"))?["long_name"]?.ToString();
-                        counter++;
-                    }
-
-                    welltop.City = city;
-                }
+				if (!string.IsNullOrEmpty(geocodingData))
+				{
+					while (city == null)
+					{   //Koordinatlara göre şehir ataması
+						JObject jsonObject = JObject.Parse(geocodingData);
+						city = jsonObject["results"][counter]["address_components"]
+															 .FirstOrDefault(c => c["types"].Any(t => t.ToString() == "locality" || t.ToString() == "administrative_area_level_1"))?["long_name"]?.ToString();
+						counter++;
+					}
+					welltop.City = city;
+				}
                 var user = await _userManager.GetUserAsync(User);
-
                 welltop.UserId = user.Id.ToString();
 
                 _dbContext.WellTops.Update(welltop);
-                _dbContext.SaveChanges();
-            }
-            catch (Exception ex)
+				_dbContext.SaveChanges();
+			}
+			catch (Exception ex) 
             {
-                BasicNotification("Koordinatlarınızı kontrol ediniz..", NotificationType.Question, "Türk deniz sahasını aştınız!");
-            }
-            return RedirectToAction("AddedWell", "ViewWelltops");
-
+				BasicNotification("Koordinatlarınızı kontrol ediniz..", NotificationType.Question, "Türk deniz sahasını aştınız!");
+			}
+            return RedirectToAction("AddedWelltops", "Home");
         }
+
         [AcceptVerbs("GET", "POST")]
         public IActionResult HasWellName(string name)
-        {
+        {   //Dinamik isim kontrolü
             var anyName = _dbContext.WellTops.Any(x => x.Name == name);
             if (anyName)
             {
@@ -269,6 +256,5 @@ namespace TpaoProject1.Controllers
                 return Json(true);
             }
         }
-
     }
 }
